4dc5c7048b3ed347afc3ff312ce185a30b6efd73
anasallahham <anastennis12@gmail.com>
2023-06-19 00:34:47 +0300

    vector and constant file

---
+++ src/physics/constants.js
@@ -0,0 +1,6 @@
+class Constants {
+    static earthRaduis = 6371;//in km 
+    static GravitationalConstant = 6.67428;
+    static earthMass = 5.9722e10;//in kg
+    static idealGas = 8.314 ;//J/(mol*K)
+}
---
+++ src/physics/vector.js
@@ -0,0 +1,47 @@
+class Vector3 {
+    constructor(x, y, z) {
+      this.x = x;
+      this.y = y;
+      this.z = z;
+    }
+    //create a vector
+    createVector(x, y, z) {
+        return new Vector3(x, y, z);
+    }
+
+    //adding two vectors
+    add(vec) {
+      return new Vector3(this.x + vec.x, this.y + vec.y, this.z + vec.z);
+    }
+    //subtracting two vectors
+    subtract(vec) {
+      return new Vector3(this.x - vec.x, this.y - vec.y, this.z - vec.z);
+    }
+    //dividing two vectors
+    divideVector(vec) {
+        return new Vector3(this.x / vec.x, this.y / vec.y, this.z / vec.z);
+    }
+    //multiply vector by a value
+    multiplyVal(value) {
+      return new Vector3(this.x * value, this.y * value, this.z * value);
+    }
+    //dividing two vectors
+    divide(value) {
+        if (value === 0) {
+          return new Vector3(0, 0, 0);
+        }
+        return new Vector3(this.x / value, this.y / value, this.z / value);
+      }
+    //length
+    length() {
+      return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+    }
+    //normal of a vector
+    normalize() {
+      const length = this.length();
+      if (length === 0) {
+        return new Vector3(0, 0, 0);
+      }
+      return new Vector3(this.x / length, this.y / length, this.z / length);
+    }
+  }
